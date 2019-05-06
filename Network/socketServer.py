5b93c2628241fac8504ac9b7c5525bded523d1d1
Jelldo <astrokwak@gmail.com>
2019-05-06 23:36:15 +0900

    modify var names

--- Network/socketServer.py
+++ Network/socketServer.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-def getRequestedFile(req: bytes):
+def get_requested_file(req: bytes):
     '''
     :param req: header
     :return: requested filename
@@ -15,7 +15,7 @@ def getRequestedFile(req: bytes):
     return ''
 
 
-def getHTTPMethod(req: bytes):
+def get_HTTP_method(req: bytes):
     '''
     :param req: header
     :return: HTTP Method (GET, POST, PUT, ...)
@@ -27,7 +27,7 @@ def getHTTPMethod(req: bytes):
     return ''
 
 
-def getHostandPort(req: bytes):
+def get_host_and_port(req: bytes):
     '''
     :param req: header
     :return: tuple(hostIP, hostPort)
@@ -38,29 +38,28 @@ def getHostandPort(req: bytes):
             return line.split()[1].split(':')[0], line.split()[1].split(':')[1]
 
 
-def getResponseMsg(statusCode: int, contentLength: int = 0):
-    if statusCode == 200:
+def get_response_msg(status_code: int, content_length: int = 0):
+    if status_code == 200:
         return 'HTTP/1.0 {} OK\r\nConnection: close\r\nID: 20165149\r\nName: Taeyoung Kwak\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n'.format(
-            statusCode, contentLength).encode()
-    elif statusCode == 404:
+            status_code, content_length).encode()
+    elif status_code == 404:
         return 'HTTP/1.0 {} NOT FOUND\r\nConnection: close\r\nID: 20165149\r\nName: Taeyoung Kwak\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n'.format(
-            statusCode, contentLength).encode()
+            status_code, content_length).encode()
 
 
-def main(serverPort: int):
+def main(server_port: int):
     BUF_SIZE = 512
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('localhost', serverPort))
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('localhost', server_port))
     ### queue up to 1
-    serverSocket.listen(1)
+    server_socket.listen(1)
     print('Server started, ready to listen')
 
     while True:
         ### establish a connection
-        connectionSocket, addr = serverSocket.accept()
-
+        connection_socket, addr = server_socket.accept()
         try:
-            serverRecv = connectionSocket.recv(BUF_SIZE)
+            serverRecv = connection_socket.recv(BUF_SIZE)
         except Exception as e:
             print(e)
             continue
@@ -70,39 +69,40 @@ def main(serverPort: int):
             continue
 
         print("\r\n" + serverRecv.decode())
-        path = getRequestedFile(serverRecv)
+        path = get_requested_file(serverRecv)
         filename = path[1:]
 
         ### check HTTP method
-        if getHTTPMethod(serverRecv) != 'GET':
-            print('Cannot {} {}'.format(getHTTPMethod(serverRecv), path))
+        if get_HTTP_method(serverRecv) != 'GET':
+            print('Cannot {} {}'.format(get_HTTP_method(serverRecv), path))
             continue
         # hostName, hostPort = getHostandPort(serverRecv)
 
         ### check file path
         if not os.path.exists(filename):
-            statusCode = 404
-            connectionSocket.send(getResponseMsg(statusCode))
+            status_code = 404
+            connection_socket.send(get_response_msg(status_code))
             print('Server Error : No such file {}!'.format(path))
         else:
-            statusCode = 200
-            dataTransferred = 0
-            contentLength = os.path.getsize(filename)
-            connectionSocket.send(getResponseMsg(statusCode, contentLength))
-            f = open(filename, 'rb')
-            data = f.read(BUF_SIZE)
-            while data:
-                try:
-                    dataTransferred += connectionSocket.send(data)
-                    data = f.read(BUF_SIZE)
-                except Exception as e:
-                    print(e)
-            f.close()
-            print('finish {} {}'.format(dataTransferred, contentLength))
-        connectionSocket.close()
-    serverSocket.close()
+            status_code = 200
+            data_transferred = 0
+            content_length = os.path.getsize(filename)
+            connection_socket.send(get_response_msg(status_code, content_length))
+            with open(filename, 'rb') as f:
+                data = f.read(BUF_SIZE)
+                while data:
+                    try:
+                        data_transferred += connection_socket.send(data)
+                        data = f.read(BUF_SIZE)
+                    except Exception as e:
+                        print(e, "Error while sending data")
+                        break
+            print('finish {} {}'.format(data_transferred, content_length))
+        connection_socket.close()
+    server_socket.close()
 
 
 if __name__ == '__main__':
+    print('Student ID : 20165149')
+    print('Name : Taeyoung Kwak')
     main(int(sys.argv[1]))
-    # main(10000)
